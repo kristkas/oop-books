be3b453c354ace3a38ea8c0b206b3c2cf94f1811
kristkas <kristin.kasuk@gmail.com>
2021-12-08 11:27:30 +0200

    refactored addBook functional by LS

--- classes/LS.js
+++ classes/LS.js
@@ -1,12 +1,21 @@
 class LS{
-    addBook(book){
-        let books
-        if(localStorage.getItem("books") === null){
-            books = []
+    getData(key){
+        let data
+        if(localStorage.getItem("key") === null){
+            data = []
         } else {
-            book = JSON.parse(localStorage.getItem("books"))
+            data = JSON.parse(localStorage.getItem("key"))
         }
-        book.push(book)
-        localStorage.setItem("books", JSON.stringify(books))
+        return data
+    }
+
+    setData(key, data){
+        localStorage.setItem(key, JSON.stringify(data))
+    }
+
+    addBook(book){
+        const books = this.getData("books")
+        books.push(book)
+        this.setData("books", books)
     }
 }

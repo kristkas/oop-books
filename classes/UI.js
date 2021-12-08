2cfed9f3bb31b23eb98cdf0431f07ba68f6ccc52
kristkas <kristin.kasuk@gmail.com>
2021-12-08 10:57:25 +0200

    added addBook functional by ui

---
+++ classes/UI.js
@@ -0,0 +1,12 @@
+class UI{
+    addBook(book){
+        const tr = document.createElement("tr")
+        const trContent = ` <td>$(book.title)</td>
+                            <td>$(book.author)</td>
+                            <td>$(book.isbn)</td>
+                            <td><a href="#">X</a></td> `
+        tr.innerHTML = trContent
+        const bookList = document.querySelector("#book-list")
+        bookList.appendChild(tr)
+    }
+}
